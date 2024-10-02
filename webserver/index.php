d92f5aed18ca2c36941b80f3dbff7528d9bc51be
alentomjames <145158385+alen-tom-james@users.noreply.github.com>
2024-10-02 13:09:23 -0400

    updated index.php to check if userID exists and display the correct navbar options

--- webserver/index.php
+++ webserver/index.php
@@ -2,14 +2,9 @@
 // Start the session
 session_start();
 $_SESSION["userID"] = "1";
-$_SESSION["loggedIn"] = false;
 // Check if the user is logged in by checking if they have a session token stored in the session storage 
-if (isset($_SESSION['userID'])) {
-    $_SESSION["loggedIn"] = true;
-    echo $_SESSION['userID'];
-  }  else {
-    echo['NOT WORKING'];
-  }
+$loggedIn = isset($_SESSION['userID']);
+
 // If they aren't logged in then display the buttons for login or sign up on the navbar
 
 // If they are logged in then display a "Welcome [user]" text at the top where the buttons would usually be 
@@ -28,33 +23,18 @@ if (isset($_SESSION['userID'])) {
     <script src="script.js" defer></script>
 </head>
 <body>
-    <!-- Add a php if statement that changes the navbar based on if theres a session id and change it to display the welcome user---> 
-	<? php 
-        if ($_SESSION['loggedIn'] == true){
-
-    ?>
+    <!-- Add a php if statement that changes the navbar based on if theres a session id and change it to display the welcome user and login --->
     <nav class="navbar">
 		<a href="index.php" class="nav-title">BreadWinners</a>
         <ul class="nav-links">
+            <?php if ($loggedIn): ?>
+                <p class="nav-title">Welcome, <?php echo $_SESSION['user_id']; ?>!</p>
+                <li><button onclick="location.href='logout.php'">Logout</button></li>
+            <?php else: ?>
             <li><button onclick="location.href='login.php'">Login</button></li>
             <li><button onclick="location.href='sign_up.php'">Sign Up</button></li>
+            <?php endif; ?>
         </ul>
     </nav>
-
-    <?php
-        } else {
-    ?>
-    <nav class="navbar">
-		<a href="index.php" class="nav-title">BreadWinners</a>
-        <ul class="nav-links">
-            <p class="nav-title">Welcome User</p>
-            <li><button onclick="location.href='logout.php'">Logout</button></li>
-            <li><button onclick="location.href='sign_up.php'">Sign Up</button></li>
-        </ul>
-    </nav>
-    <?php
-        } 
-    ?>
-
 </body>
 </html>
