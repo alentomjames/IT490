6061bb8f8817403d6e58da1d10e3df0c936ecb95
Philzerin <philzerin530@gmail.com>
2024-10-09 20:25:00 -0400

    edited mysqlconnect

---
+++ db_connection.php
@@ -0,0 +1,36 @@
+#!/usr/bin/php
+<?php
+
+$getenv = parse_ini_file('db_config.env');
+
+function getDbConnection()
+{
+	global $getenv;
+	$host = $getenv['DB_HOST'];
+	$username = $getenv['DB_USER'];
+	$password = $getenv['DB_PASSWORD'];
+	$database = $getenv['DB_NAME'];
+
+	$mydb = new mysqli($host, $username, $password, $database);
+
+	if ($mydb->connect_errno) {
+		echo "Failed to connect to database: " . $mydb->connect_error . PHP_EOL;
+		exit(0);
+	} else {
+		echo "Successfully connected to database" . PHP_EOL;
+	}
+
+	return $mydb;
+}
+
+// $query = "select * from users;";
+
+// $response = $mydb->query($query);
+// if ($mydb->errno != 0)
+// {
+// 	echo "failed to execute query:".PHP_EOL;
+// 	echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+// 	exit(0);
+// }
+
+?>
