dbfc02ebb1a3413a373d3b6f9c2e78116141267e
Anthony <al643@njit.edu>
2024-10-10 11:17:49 -0400

    Had to remove some leftover lines that don't help with the DMZ's operation

--- DMZ.sh
+++ DMZ.sh
@@ -1,4 +1,4 @@
-#! / Bin / bash
+#!/bin/bash
 
 #BASIC RULES 
 
@@ -19,23 +19,26 @@ iptables  -A  OUTPUT  -j  ACCEPT
 
 #ETH0 = Connects to the Router
 #ETH1 = Connects to the DMZ
-#ETH2 = Connects to the Local Server?
+#ETH2 = Connects to the Local Serverr?
+#We are not using ethernet, so these lines are voided
 
 # LET'S ALL TRAFFIC GO FROM ETH0 TO ETH1 
 
-iptables  -A  FORWARD  -i  eth0  -o  eth1  -s  state  --state  NEW, ESTABLISHED, RELATED  -j  ACCEPT
+# sudo iptables  -A  FORWARD  -i  eth0  -o  eth1  -s  state  --state  NEW, ESTABLISHED, RELATED  -j  ACCEPT
 
 # LET'S THE RESPONSE ONLY GO TO PETITIONS FROM ETH1 TO ETH0 
 
-iptables  -A  FORWARD  -i  eth1  -o  eth0  -s  state  --state  ESTABLISHED, RELATED  -j  ACCEPT
+# sudo iptables  -A  FORWARD  -i  eth1  -o  eth0  -s  state  --state  ESTABLISHED, RELATED  -j  ACCEPT
 
 # LET'S ALL TRAFFIC GO FROM ETH2 TO ETH1 
 
-iptables  -A  FORWARD  -i  eth2  -o  eth1  -s  state  --state  NEW, ESTABLISHED, RELATED  -j  ACCEPT
+# sudo iptables  -A  FORWARD  -i  eth2  -o  eth1  -s  state  --state  NEW, ESTABLISHED, RELATED  -j  ACCEPT
 
 # LETS ONLY PASSES RESPONSES TO PETITIONS FROM ETH1 TO ETH2 
 
-iptables  -A  FORWARD  -i  eth1  , or  eth2  -s  state  --state  ESTABLISHED, RELATED  -j  ACCEPT
+# sudo iptables  -A  FORWARD  -i  eth1  , or  eth2  -s  state  --state  ESTABLISHED, RELATED  -j  ACCEPT
+
+
 
 #PRIORITIZE REDIRECTIONS FROM THE OUTSIDE TO THE DMZ
 
@@ -43,7 +46,7 @@ iptables  -A  FORWARD  -i  eth1  , or  eth2  -s  state  --state  ESTABLISHED, RE
 
 #TCP Port 1414, Connect to the Message Router (may or may not work)
 
-iptables  -t  nat  -A  PREROUTING  -i  eth0  -p  tcp  --dport  53  -j  DNAT  --to  192.168.2.4: 1414
+sudo iptables  -t  nat  -A  PREROUTING  -i  eth0  -p  tcp  --dport  53  -j  DNAT  --to  192.168.2.4: 1414
 
 
 #alternative prerouting lines
