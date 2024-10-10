2872b564f03de6514652585b26f42998165270c7
alentomjames <145158385+alen-tom-james@users.noreply.github.com>
2024-10-10 10:43:33 -0400

    deleting time limit

--- webserver/login_chk.php
+++ webserver/login_chk.php
@@ -64,7 +64,6 @@ function receiveRabbitMQResponse(){
 
       // Wait for the response
       while ($channel->is_consuming()) {
-        set_time_limit(30);
         $channel->wait();
         
     }
