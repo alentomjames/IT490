2f14c9e610a54658087d4cfbadce2eb8b4973e3b
Da Ngelo <philzerin530@gmail.com>
2024-10-22 16:53:35 -0400

    Debugging

--- auth/login.php
+++ auth/login.php
@@ -54,7 +54,12 @@ function login(string $username, string $password)
                 'userID'  => $id,
                 //'sessionID' => $sessionId
             ]);
+            $stmt->close();
+            $dbConnection->close();
+            echo "Closed connection\n";
             echo "Login successful for user: $username\n";
+            echo $response . "\n";
+            return $response;
         } else {
             // when passwords dont match, send failure response
             $response = json_encode(['type' => 'failure']);
