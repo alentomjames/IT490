b10475db2928411151403df9e119830fd8d32f36
alentomjames <145158385+alen-tom-james@users.noreply.github.com>
2024-10-28 16:13:56 -0400

    createed api_handler which is listening for an incoming request, takes logic from apiCall2 that Anthony made and puts it into the same switch function, fetchDetails calls the API and returns the body which is sent to the user

--- api_handler.php
+++ api_handler.php
@@ -1,49 +1,39 @@
 <?php
 
-require_once 'db_connection.php'; // file has db connection
 require_once 'webserver/rabbitmq_connection.php'; // how I connect to RabbitMQ
 require_once  'webserver/vendor/autoload.php';
 
 use PhpAmpqLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-require_once 'apiCall2php';
+$client = new \GuzzleHttp\Client();
 
 // get the rabbitmq connection
 list($connection, $channel) = getRabbit();
 
-// queue where i'll consume login/register requests
+// Listening on the frontEnd queue for the type of API requests needed 
 $channel->queue_declare('frontendQueue', false, true, false, false);
 
 // process the login/register requests
 $callback = function ($msg) use ($channel) {
     $data = json_decode($msg->body, true);
-    $type = $data['type']; // for now types are: login, register
-    $username = $data['username'];
-    $password = $data['password'];
-    $movieId = $data['movie_id'];
-    $userId = $data['user_id'];
+    $type = $data['type']; // for now types are: movie_details
+    $parameter = $data['parameter'];
 
-    if ($type === 'login') {
-        // calling login function
-        $response = login($username, $password);
-    } elseif ($type === 'register') {
-        // register request
-        $name = $data['name'];
-        $response = register($name, $username, $password);
-    } elseif ($type === "add_to_watchlist") {
-        // add to watchlist
-        $response = addToWatchlist($movieId, $userId);
-    } elseif ($type === "remove_from_watchlist") {
-        // remove from watchlist
-        $response = removeFromWatchlist($movieId, $userId);
-    } elseif ($type === "get_watchlist") {
-        // get all watchlist
-        $response = getFromWatchlist($userId);
+    switch ($type) {
+        case 'movie_details':
+            $url = "https://api.themoviedb.org/3/movie/{$parameter}?language=en-US";
+            $response = fetchDetails($type, $parameter, $url);
+            break;
+        case 'person_details':
+            $url = "https://api.themoviedb.org/3/person/{$parameter}?language=en-US";
+            $response = fetchDetails($type, $parameter, $url);
+            break;
     }
+    
     // send the response back to the client
     $responseMsg = new AMQPMessage($response, ['delivery_mode' => 2]);
-    $channel->basic_publish($responseMsg, 'directExchange', 'databaseQueue');
+    $channel->basic_publish($responseMsg, 'directExchange', 'dmzQueue');
 };
 
 
@@ -59,3 +49,18 @@ while ($channel->is_consuming()) {
 closeRabbit($connection, $channel);
 
 // userid, sessionID, timestamp
+
+function fetchDetails ($type, $parameter, $url) {
+    // Call the API to get a response 
+    $response = $client->request('GET', $url, [
+        'headers' => [
+          'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmZiYTg5YTMyMzE3MmRmZmE0Mjk5NjU3YTM3MTYzNyIsIm5iZiI6MTcyOTE3NDI3NS44MTA5NTUsInN1YiI6IjY3MTExYThiY2Y4ZGU4NzdiNDlmY2JlMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3wTZmroEtn8GSIHD92r7p-3G4iAjPMHZa3aojxycDIM',
+          'accept' => 'application/json',
+        ],
+      ]);
+
+      $responseBody = json_decode($response->getBody(), true);
+      //send back to RabbitMQ
+
+      return $responseBody;
+}
