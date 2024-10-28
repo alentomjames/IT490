ea9d5fb762c023bbcf5ba3ce09584b1ee5f95a95
Da Ngelo <philzerin530@gmail.com>
2024-10-27 22:57:29 -0400

    Added watchlist functionality. Pushing from desktop

--- movies/watchlist.php
+++ movies/watchlist.php
@@ -6,43 +6,50 @@ require_once 'rmq_connection.php'; // how I connect to RabbitMQ
 
 $dbConnection = getDbConnection();
 
-// add to watchlist paramter movie id user id
 function addToWatchlist(int $movieId, int $userId)
 {
     global $dbConnection;
 
-    $query = ('INSERT INTO watchlist (movie_id, user_id) VALUES (?, ?)');
+    $query = 'INSERT INTO watchlist (movie_id, user_id) VALUES (?, ?)';
     $stmt = $dbConnection->prepare($query);
     $stmt->bind_param("ii", $movieId, $userId);
+
     try {
-        //code...
         $stmt->execute();
-    } catch (\Throwable $th) {
-        //throw $th;
-        echo "Wrong movie_id format\n";
+        if ($stmt->affected_rows > 0) {
+            $response = ['type' => 'success', 'message' => 'Movie added to watchlist'];
+        } else {
+            $response = ['type' => 'failure', 'message' => 'Failed to add movie to watchlist'];
+        }
+    } catch (mysqli_sql_exception $e) {
+        if ($e->getCode() == 1062) { // duplicate entry error code
+            $response = ['type' => 'failure', 'message' => 'Movie already exists in watchlist'];
+        } else {
+            $response = ['type' => 'failure', 'message' => 'Error adding movie: ' . $e->getMessage()];
+        }
     }
 
-    $stmt->store_result();
-
-    if ($stmt->num_rows() > 0) {
-        echo "Error, movie already exists in watchlist\n";
-    }
+    $stmt->close();
+    return $response;
 }
 
 function removeFromWatchlist(int $movieId, int $userId)
 {
-
     global $dbConnection;
 
-    $query = ('DELETE FROM watchlist WHERE movie_id = ? AND user_id = ?');
+    $query = 'DELETE FROM watchlist WHERE movie_id = ? AND user_id = ?';
     $stmt = $dbConnection->prepare($query);
     $stmt->bind_param("ii", $movieId, $userId);
     $stmt->execute();
-    $stmt->store_result();
 
-    if ($stmt->affected_rows == 0) {
-        echo "Error, movie does not exist in watchlist\n";
+    if ($stmt->affected_rows > 0) {
+        $response = ['type' => 'success', 'message' => 'Movie removed from watchlist'];
+    } else {
+        $response = ['type' => 'failure', 'message' => 'Movie does not exist in watchlist'];
     }
+
+    $stmt->close();
+    return $response;
 }
 
 function getFromWatchlist(int $userId)
@@ -57,23 +64,25 @@ function getFromWatchlist(int $userId)
 
     $watchlist = [];
     while ($row = $result->fetch_assoc()) {
-        $watchlist[] = $row['movie_id']; // collects a user's watchlist
+        $watchlist[] = $row['movie_id'];
     }
 
     $stmt->close();
-    return $watchlist;
+
+    if (!empty($watchlist)) {
+        $response = ['type' => 'success', 'watchlist' => $watchlist];
+    } else {
+        $response = ['type' => 'failure', 'message' => 'Watchlist is empty'];
+    }
+
+    return $response;
 }
 
-//addToWatchlist('movie1', 1);
-addToWatchlist(222, 1);
-addToWatchlist(333, 1);
-addToWatchlist(333, 2);
-addToWatchlist(333, 2);
+// Example usage and output
+$responseAdd = addToWatchlist(222, 1);
+$responseRemove = removeFromWatchlist(222, 1);
+$responseGet = getFromWatchlist(1);
 
-//removeFromWatchlist('movie1', 1);
-
-//getFromWatchlist(1);
-
-$userWatchlist = getFromWatchlist(2);
-
-var_dump($userWatchlist);
+var_dump($responseAdd);
+var_dump($responseRemove);
+var_dump($responseGet);
--- query_handler.php
+++ query_handler.php
@@ -9,6 +9,7 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 require_once 'auth/login.php';
 require_once 'auth/register.php';
+require_once 'movies/watchlist.php';
 
 // get the rabbitmq connection
 list($connection, $channel) = getRabbit();
@@ -22,6 +23,8 @@ $callback = function ($msg) use ($channel) {
     $type = $data['type']; // for now types are: login, register
     $username = $data['username'];
     $password = $data['password'];
+    $movieId = $data['movie_id'];
+    $userId = $data['user_id'];
 
     if ($type === 'login') {
         // calling login function
@@ -30,6 +33,15 @@ $callback = function ($msg) use ($channel) {
         // register request
         $name = $data['name'];
         $response = register($name, $username, $password);
+    } elseif ($type === "add_to_watchlist") {
+        // add to watchlist
+        $response = addToWatchlist($movieId, $userId);
+    } elseif ($type === "remove_from_watchlist") {
+        // remove from watchlist
+        $response = removeFromWatchlist($movieId, $userId);
+    } elseif ($type === "get_watchlist") {
+        // get all watchlist
+        $response = getFromWatchlist($userId);
     }
     // send the response back to the client
     $responseMsg = new AMQPMessage($response, ['delivery_mode' => 2]);
