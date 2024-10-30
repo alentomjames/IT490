57bd4f346576e120ddcb0e1e771cfa9cd0d4b6bd
Da Ngelo <philzerin530@gmail.com>
2024-10-30 01:54:53 -0400

    lets goo

--- webserver/removeFromWatchlist.php
+++ webserver/removeFromWatchlist.php
@@ -43,6 +43,9 @@ if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
 function receiveRabbitMQResponse($connection, $channel)
 {
 
+    if (ob_get_length()) {
+        ob_clean();
+    }
     $channel->queue_declare('databaseForFrontend', false, true, false, false);
 
     $callback = function ($msg) {
