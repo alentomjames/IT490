820ef9123e1b30afe63e561afca1345160566518
Philzerin <philzerin530@gmail.com>
2024-10-30 18:58:44 -0400

    fix

--- webserver/fetchWatchlist.php
+++ webserver/fetchWatchlist.php
@@ -47,7 +47,7 @@ function receiveRabbitMQResponse($connection, $channel)
     $callback = function ($msg) {
         $response = json_decode($msg->body, true);
         if ($response['type'] === 'success') {
-            echo json_encode(['type' => 'success', 'watchlist' => $response]);
+            echo json_encode(['type' => 'success', 'watchlist' => $response['watchlist']]);
         } else {
             echo json_encode(['type' => 'failure', 'message' => 'Failed to retrieve watchlist']);
         }
