39abf6ecf15c84a87b828640f19463ca3c5b0254
Da Ngelo <philzerin530@gmail.com>
2024-10-29 23:51:54 -0400

    getting ready to add watchlist functionality in prod

--- frontend/removeFromWatchlist.php
+++ frontend/removeFromWatchlist.php
@@ -1,107 +1,68 @@
 <?php
 
-
 session_start();
 header('Content-Type: application/json');
-//require_once './rmq_connection.php';
-//require_once './vendor/autoload.php';
 require_once '../vendor/autoload.php';
-require_once '../db_connection.php'; // file has db connection
-require_once '../rmq_connection.php'; // how I connect to RabbitMQ
-
-require_once '../movies/watchlist.php';
+require_once '../db_connection.php';
+require_once '../rmq_connection.php';
 
 use PhpAmqpLib\Message\AMQPMessage;
 
-$userId = $_SESSION['userID'] = 1; // Hardcoded user ID for testing
-// $loggedIn = isset($userId);
-// if (!isset($_SESSION['userID'])) {
-//     echo json_encode(['success' => false, 'message' => 'User not logged in']);
-//     exit;
-// }
-
-
-
 if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
-    if (isset($_SESSION['userID'])) {
-        $input = json_decode(file_get_contents('php://input'), true);
-        $movieId = $input['movie_id'];
-        $userId = $_SESSION['userID'];
-        $type = "remove_from_watchlist";
-        $response = removeFromWatchlist($movieId, $userId);
-        // Encode the response as JSON
-        echo json_encode($response);
-    } else {
+
+    if (!isset($_SESSION['userID'])) {
         echo json_encode(['type' => 'failure', 'message' => 'User not logged in']);
+        exit;
     }
+
+
+    $input = json_decode(file_get_contents('php://input'), true);
+    $movieId = $input['movie_id'];
+    $userId = $_SESSION['userID'];
+    $type = "remove_from_watchlist";
+
+    list($connection, $channel) = getRabbit();
+
+
+    $channel->queue_declare('frontendForDB', false, true, false, false);
+
+
+    $data = json_encode([
+        'type'     => $type,
+        'movie_id' => $movieId,
+        'user_id'  => $userId
+    ]);
+
+    $msg = new AMQPMessage($data, ['delivery_mode' => 2]);
+    $channel->basic_publish($msg, 'directExchange', 'frontendForDB');
+
+
+    receiveRabbitMQResponse($connection, $channel);
 }
 
-// $input = json_decode(file_get_contents('php://input'), true);
-// $movieId = $input['movie_id'];
-// $userId = $_SESSION['userID'];
-// $type = 'add_to_watchlist';
-
-// list($connection, $channel) = getRabbit();
-
-// $channel->queue_declare('frontendQueue', false, true, false, false);
-
-// $data = json_encode([
-//     'type'     => $type,
-//     'movie_id' => $movieId,
-//     'user_id'  => $userId
-// ]);
-
-// $msg = new AMQPMessage($data, ['delivery_mode' => 2]);
-// $channel->basic_publish($msg, 'directExchange', 'frontendQueue');
-// closeRabbit($connection, $channel);
-
-// receiveRabbitMQResponse();
-//}
-// list($connection, $channel) = getRabbit();
-// $channel->queue_declare('frontendForDB', false, true, false, false);
-
-$data = json_encode([
-    'type'   => 'get_watchlist',
-    'user_id' => $userId
-]);
-
-// $msg = new AMQPMessage($data, ['delivery_mode' => 2]);
-// $channel->basic_publish($msg, 'directExchange', 'frontendForDB');
-// closeRabbit($connection, $channel);
-
-
-//echo ($response['watchlist']);
-
-
-function receiveWatchlistResponse()
+function receiveRabbitMQResponse($connection, $channel)
 {
-    $response = getFromWatchlist($_SESSION['userID']);
-    return ($response);
-    // list($connection, $channel) = getRabbit();
-    // $channel->queue_declare('databaseForFrontend', false, true, false, false);
 
-    // $watchlist = [];
+    $channel->queue_declare('databaseForFrontend', false, true, false, false);
 
-    // $callback = function ($msg) use (&$watchlist) {
-    //     $response = json_decode($msg->body, true);
-    //     if (isset($response['type']) && $response['type'] === 'success' && isset($response['watchlist'])) {
-    //         echo "Watchlist is hrere";
-    //         $watchlist = $response['watchlist'];
-    //         echo json_encode(['success' => true, 'watchlist' => $watchlist]);
-    //     } else {
-    //         echo "wata";
-    //         echo json_encode(['success' => false, 'message' => 'Failed to retrieve watchlist']);
-    //     }
-    //     exit();
-    // };
+    $callback = function ($msg) {
+        $response = json_decode($msg->body, true);
 
-    // // $channel->basic_consume('databaseForFrontend', '', false, true, false, false, $callback);
+        if ($response['type'] === 'success') {
+            echo json_encode(['type' => 'success', 'message' => 'Movie removed from watchlist']);
+        } else {
+            echo json_encode(['type' => 'failure', 'message' => 'Failed to remove movie from watchlist']);
+        }
+        exit;
+    };
 
-    // // while ($channel->is_consuming()) {
-    // //     $channel->wait();
-    // // }
 
-    // // closeRabbit($connection, $channel);
-    // echo "wda";
-    // return json_encode(['success' => true, 'watchlist' => $watchlist]);
+    $channel->basic_consume('databaseForFrontend', '', false, true, false, false, $callback);
+
+
+    while ($channel->is_consuming()) {
+        $channel->wait();
+    }
+
+    closeRabbit($connection, $channel);
 }
