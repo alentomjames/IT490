9f99321144434890fb1a258d611103bb0e4d65e3
Da Ngelo <philzerin530@gmail.com>
2024-10-30 15:40:52 -0400

    fix

--- movies/likedMovies.php
+++ movies/likedMovies.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once './webserver/vendor/autoload.php';
-require_once './db_connection.php'; // file has db connection
-require_once './webserver/rabbitmq_connection.php'; // how I connect to RabbitMQ
+require_once './vendor/autoload.php';
+require_once 'db_connection.php';
+require_once 'rmq_connection.php';
 
 $dbConnection = getDbConnection();
 
