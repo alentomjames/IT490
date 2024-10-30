726222cf7a19a882bcc83d47a4e1be817ffacc22
username <MY_NAME@example.com>
2024-10-30 02:07:54 -0400

    added discover movie details case

--- api_handler.php
+++ api_handler.php
@@ -48,6 +48,14 @@ $callback = function ($msg) use ($channel) {
             echo "Fetching reccomendations: $url\n";
             $response = fetchDetails($type, $parameter, $url);
             break;
+
+        case 'discover_movie_details':
+            // https://api.themoviedb.org/3/discover/movie
+            // request = https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc
+            $url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page={$parameter}";
+            echo "Fetching discover movie details for URL: $url\n";
+            break; 
+            
         default:
             echo "Unrecognized type: $type\n";
             return;
