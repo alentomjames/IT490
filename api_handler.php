8ff868560e0da8e5228c55fcc42e073a75c7f6ec
username <MY_NAME@example.com>
2024-10-30 04:13:27 -0400

    re-adding parameter, replacing it as the time_window

--- api_handler.php
+++ api_handler.php
@@ -69,10 +69,9 @@ $callback = function ($msg) use ($channel) {
         case 'trending_movies':
             // https://api.themoviedb.org/3/trending/movie/{time_window}
             // request = https://api.themoviedb.org/3/trending/movie/day?language=en-US
-            $url = "https://api.themoviedb.org/3/trending/movie/day?language=en-US";
+            $url = "https://api.themoviedb.org/3/trending/movie/{$parameter}?language=en-US";
             // parameter = time_window (day)
             echo "Fetching trending movie details for URL: $url\n";
-            $parameter = 'placeholder';
 	        $response = fetchDetails($type, $parameter, $url);
             break; 
 
