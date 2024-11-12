f84e20648c46f802d205c275677a9876f9c18e0a
username <MY_NAME@example.com>
2024-11-11 19:10:18 -0500

    minor edit for api_handlerErrorLogGenerator

--- DistributedLogging/api_handlerErrorLogGenerator.php
+++ DistributedLogging/api_handlerErrorLogGenerator.php
@@ -2,4 +2,11 @@
 
 // Creates api_handler error logs that tell of any issues that occurred when the api_handler was running.
 
+require_once 'webserver/rabbitmq_connection.php';
+require_once 'webserver/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+use GuzzleHttp\Exception\RequestException;
+
 ?>
