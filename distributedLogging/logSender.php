9c66e485912115d25b92e95e92f36b9a560ae7ba
username <MY_NAME@example.com>
2024-11-11 21:09:49 -0500

    logSender.php update

--- distributedLogging/logSender.php
+++ distributedLogging/logSender.php
@@ -1,27 +1,24 @@
 <?php
-    require_once 'webserver/rabbitmq_connection.php';
+    require_once 'rabbitmq_connection.php';
 
     //Adjust this path variable to what where your guys error logs are
     $logFile = '/var/log/apache2/error.log'; 
     $machineName = 'DMZ';
 
-    // Amount of seconds between checking for logs 
-    $logInterval = 5; 
-
     $logs = [];
     
     $file = fopen($logFile, 'r');
-    fseek($file, 0, SEEK_END);
+    //fseek($file, 0, SEEK_END);
 
-    echo "Starting log monitoring";
-    
-    while(true){
+    echo "Starting log monitoring for $file\n";
+
+    while(true) {
         $line = fgets($file);
         if ($line !== false ){
-            
+            echo "Read line from Apache log: $line\n";  
             $timestamp = date("F j, Y, g:i a");
             $logEntry = trim($line);
-            echo "Error recieved from Apache Server";
+            echo "Error recieved from Apache Server\n";
 
             if (isset($logs[$logEntry])) {
                 // Counts the amount of times the error showed up
@@ -32,7 +29,7 @@
                     'count' => 1,
                     'timestamp' => $timestamp
                 ];
-            };
+            }
 
         $logJSON = json_encode([
             'machine' => $machineName,
@@ -40,13 +37,11 @@
             'message' => $logs[$logEntry]['message'],
             'count' => $logs[$logEntry]['count']
         ]);
+        echo "Error sent from Apache Server to Distrubted Logger: $logJSON\n";
 
         sendLog($logJSON);
-        echo "Error sent from Apache Server to Distrubted Logger";
-
     } 
 
-    sleep($logInterval);
-};
+}
 
 ?>
