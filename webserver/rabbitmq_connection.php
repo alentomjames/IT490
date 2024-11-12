51c4f62543e2f7b407f7223d59a6181728f389e8
username <MY_NAME@example.com>
2024-11-11 22:01:48 -0500

    Updated the rabbitmq_connection.php file in the webserver folder

--- webserver/rabbitmq_connection.php
+++ webserver/rabbitmq_connection.php
@@ -111,3 +111,43 @@ function recieveDB()
 
     return $data;
 }
+
+function sendLog($logMessage)
+{
+    list($connection, $channel) = getRabbit();
+    try {
+        $channel->exchange_declare('fanoutExchange', 'fanout', false, true, false);
+        $msg = new AMQPMessage($logMessage, ['delivery_mode' => 2]);
+        $channel->basic_publish($msg, 'fanoutExchange');
+        echo "Log message sent to fanoutExchange\n";
+    } catch (Exception $e) {
+        echo "Error publishing message to RabbitMQ: " . $e->getMessage() . "\n";
+    } finally {
+        closeRabbit($connection, $channel);
+    }
+}
+
+function recieveLogs()
+{
+    list($connection, $channel) = getRabbit();
+    $channel->queue_declare('toFeDev', false, true, false, false);
+
+    echo "Waiting for logs. To exit press CTRL+C\n";
+
+    $logPath = '/var/log/distributedLogs.txt';
+
+    $callback = function ($msg) use ($logPath) {
+        file_put_contents($logPath, $msg->body . PHP_EOL, FILE_APPEND);
+    };
+
+    $channel->basic_consume('toFeDev', '', false, true, false, false, $callback);
+
+    // Wait for the response
+    while ($channel->is_consuming()) {
+        $channel->wait();
+        echo "Error recieved from Distrubted Logger\n";
+
+    }
+    // Close the channel and connection
+    closeRabbit($connection, $channel);
+}
