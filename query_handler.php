97e40f382eadbea14e77a29a974869de50940864
Petar Petroski <petar.petroski2003@gmail.com>
2024-11-18 19:55:13 -0500

    Rename RabbitMQ connection file for consistency

--- query_handler.php
+++ query_handler.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'db_connection.php'; // file has db connection
-require_once 'rmq_connection.php'; // how I connect to RabbitMQ
+require_once 'rabbitmq_connection.php'; // how I connect to RabbitMQ
 require_once 'vendor/autoload.php';
 
 use PhpAmpqLib\Connection\AMQPStreamConnection;
