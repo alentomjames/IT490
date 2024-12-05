0606af1343a5bf411ff72a301198d99bfc493b60
Petar Petroski <petar.petroski2003@gmail.com>
2024-12-05 11:16:54 -0500

    fix: update file paths in bundleRatingAndRollback script for correct autoload and RabbitMQ connection

--- deployment/bundleRatingAndRollack.php
+++ deployment/bundleRatingAndRollack.php
@@ -1,8 +1,7 @@
 <?php
 
-require_once './webserver/vendor/autoload.php';
-require_once './db_connection.php'; // file has db connection
-require_once './webserver/rabbitmq_connection.php'; // how I connect to RabbitMQ
+require_once '../webserver/vendor/autoload.php';
+require_once '../webserver/rabbitmq_connection.php'; // how I connect to RabbitMQ
 
 use PhpAmqpLib\Message\AMQPMessage;
 
