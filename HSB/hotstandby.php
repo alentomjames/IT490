93c7628b863cd3c16aa7f8317c0d9c2d7890ccd6
alentomjames <atj2@njit.edu>
2024-12-12 14:53:20 -0500

    more echo statements

--- HSB/hotstandby.php
+++ HSB/hotstandby.php
@@ -5,8 +5,12 @@ $networkInterface = 'ztosimf46d';
 $switchIP = "sudo ip addr add $ip dev $networkInterface";
 $restartApache = "sudo systemctl restart apache2";
 
+echo "Starting IP switch\n";
+
+
 // Switch IP to the PROD Server's IP
 exec($switchIP, $output, $return_var);
+echo "Switch IP Output: \n" . implode("\n", $output) . "\n";
 if ($return_var == 0) {
     echo "Switched to Hot Standby Server.\n";
     } else {
@@ -15,7 +19,9 @@ if ($return_var == 0) {
 };
 
 // Restart Apache2
+echo "Restarting Apache\n";
 exec($restartApache, $output, $return_var);
+echo "Restart Apache Output: \n" . implode("\n", $output) . "\n";
 if ($return_var == 0) {
     echo "Apache restarted successfully.\n";
     } else {
