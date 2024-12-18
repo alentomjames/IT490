7d1268f64a2b77eb0c28cb820fdd81f0cf76859e
Petar Petroski <petar.petroski2003@gmail.com>
2024-12-18 13:32:42 -0500

    Add logging for received messages in dbHotstandby.php to enhance monitoring

--- HSB/dbHotstandby.php
+++ HSB/dbHotstandby.php
@@ -18,6 +18,7 @@ $channel->queue_declare('prodForHSB', false, true, false, false);
 
 $callback = function ($msg) use ($switchIP, $restartMySQL, $networkInterface) {
     $data = json_decode($msg->body, true);
+    echo "Received message: " . $data;
     if ($data['status'] === 'down') {
         echo "PROD Server is down. Initiating failover...\n";
 
