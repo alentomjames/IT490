0cc1d557b328785e4de360d64aac573ad6ad5205
Petar Petroski <petar.petroski2003@gmail.com>
2024-12-18 15:04:12 -0500

    Enhance dbHeartbeat.php to check database and RabbitMQ status, stopping services if either is down

--- HSB/dbHeartbeat.php
+++ HSB/dbHeartbeat.php
@@ -1,18 +1,34 @@
-#!/usr/bin/php
 <?php
+#!/usr/bin/php
 require_once '../rabbitmq_connection.php';
 require_once '../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
-list($connection, $channel) = getProdRabbit();
 
+function isDatabaseUp() {
+    $response = exec('php dbCheck.php');
+    return trim($response) === "OK";
+}
+
+function isRabbitMQUp() {
+    try {
+        list($connection, $channel) = getProdRabbit();
+        $channel->close();
+        $connection->close();
+        return true;
+    } catch (Exception $e) {
+        return false;
+    }
+}
+
+list($connection, $channel) = getDeployRabbit();
 $channel->queue_declare('prodForHSB', false, true, false, false);
 
 while (true) {
-    $response = exec('php dbCheck.php');
-    echo "Response: $response\n";
+    $dbStatus = isDatabaseUp();
+    $rabbitStatus = isRabbitMQUp();
 
-    if ($response == "OK") {
+    if ($dbStatus && $rabbitStatus) {
         echo "PROD Server is still active.\n";
         $data = json_encode([
             'type' => 'hotstandby',
@@ -26,11 +42,19 @@ while (true) {
             'type' => 'hotstandby',
             'status' => 'down'
         ]);
-
         $msg = new AMQPMessage($data, ['delivery_mode' => 2]);
         $channel->basic_publish($msg, 'directExchange', 'prodForHSB');
-        closeRabbit($connection, $channel);
 
+        // Stop the other service if one is down
+        if (!$dbStatus) {
+            echo "Database is down. Stopping RabbitMQ.\n";
+            exec('sudo systemctl stop rabbitmq-server');
+        } else {
+            echo "RabbitMQ is down. Stopping Database.\n";
+            exec('sudo systemctl stop mysql');
+        }
+
+        closeRabbit($connection, $channel);
         break;
     }
 
