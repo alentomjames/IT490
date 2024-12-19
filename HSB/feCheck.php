1eea2341e80c24752793696201b8ea466aae7af5
alentomjames <atj2@njit.edu>
2024-12-18 20:39:37 -0500

    adding logic to ignore ssl

--- HSB/feCheck.php
+++ HSB/feCheck.php
@@ -11,6 +11,11 @@ $standby = "HSB";
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+
+// Ignore SSL verification
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+
 $response = curl_exec($ch);
 $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
