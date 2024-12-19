66539850dfa6d467ac1c511a80d81603c309c83d
alentomjames <atj2@njit.edu>
2024-12-18 20:30:46 -0500

    adding many many more echo statements

--- HSB/feCheck.php
+++ HSB/feCheck.php
@@ -3,26 +3,31 @@
 $ip = '172.29.244.200';
 $url = 'https://172.29.244.200/HSB/okay.php';
 
+// Change this based on what machine you're on
 $current = "PROD";
-// $current = "HSB";
+$standby = "HSB";
 
 // Curls the okay.php page
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_TIMEOUT, 5);
 $response = curl_exec($ch);
+$responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
 $response = trim($response);
+echo "Response: $response\n"; 
 
-if ($response == "HSB ACTIVE") {
+if ($response == "$standby ACTIVE") {
     // This machine becomes the standby machine and starts the heartbeat.php function
+    echo "$standby is ACTIVE, activating heartbeat on $current\n";
     exec('php /home/alen/git/IT490/HSB/heartbeat.php', $output, $return_var);
     echo "$current is now the STANDBY machine\n";
-} else {
+} else if (empty($response) || $responseCode != 200) {
+    echo "No response from $standby\n, $current attempting to become the ACTIVE machine\n";
     exec ('php /home/alen/git/IT490/HSB/hotstandby.php', $output, $return_var);
     echo "$current is now the ACTIVE machine\n";
     // Update the okay file 
     file_put_contents('/var/www/it490/HSB/okay.php', "<?php echo \"$current ACTIVE\\n\"; ?>");
-}
+} 
 ?>
