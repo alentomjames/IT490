6aba23455e75c6d6253235881d77343a08731704
Petar Petroski <petar.petroski2003@gmail.com>
2024-12-19 14:20:06 -0500

    Replace fetchDetails call with hardcoded movie details response

--- dmzBundle/api_handler.php
+++ dmzBundle/api_handler.php
@@ -64,7 +64,50 @@ $callback = function ($msg) use ($channel) {
         case 'movie_details':
             $url = "https://api.themoviedb.org/3/movie/{$parameter}?language=en-US";
             echo "Fetching movie details for URL: $url\n";
-            $response = fetchDetails($type, $parameter, $url);
+            $response = json_encode([
+                'type' => 'success',
+                'data' => [
+                    'adult' => false,
+                    'backdrop_path' => '/hZkgoQYus5vegHoetLkCJzb17zJ.jpg',
+                    'belongs_to_collection' => null,
+                    'budget' => 63000000,
+                    'genres' => [
+                        ['id' => 18, 'name' => 'Drama']
+                    ],
+                    'homepage' => 'http://www.foxmovies.com/movies/fight-club',
+                    'id' => 550,
+                    'imdb_id' => 'tt0137523',
+                    'origin_country' => ['US'],
+                    'original_language' => 'en',
+                    'original_title' => 'Fight Club',
+                    'overview' => 'A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground "fight clubs" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.',
+                    'popularity' => 95.569,
+                    'poster_path' => '/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg',
+                    'production_companies' => [
+                        ['id' => 711, 'logo_path' => '/tEiIH5QesdheJmDAqQwvtN60727.png', 'name' => 'Fox 2000 Pictures', 'origin_country' => 'US'],
+                        ['id' => 508, 'logo_path' => '/7cxRWzi4LsVm4Utfpr1hfARNurT.png', 'name' => 'Regency Enterprises', 'origin_country' => 'US'],
+                        ['id' => 4700, 'logo_path' => '/A32wmjrs9Psf4zw0uaixF0GXfxq.png', 'name' => 'The Linson Company', 'origin_country' => 'US'],
+                        ['id' => 25, 'logo_path' => '/qZCc1lty5FzX30aOCVRBLzaVmcp.png', 'name' => '20th Century Fox', 'origin_country' => 'US'],
+                        ['id' => 20555, 'logo_path' => '/hD8yEGUBlHOcfHYbujp71vD8gZp.png', 'name' => 'Taurus Film', 'origin_country' => 'DE']
+                    ],
+                    'production_countries' => [
+                        ['iso_3166_1' => 'DE', 'name' => 'Germany'],
+                        ['iso_3166_1' => 'US', 'name' => 'United States of America']
+                    ],
+                    'release_date' => '1999-10-15',
+                    'revenue' => 100853753,
+                    'runtime' => 139,
+                    'spoken_languages' => [
+                        ['english_name' => 'English', 'iso_639_1' => 'en', 'name' => 'English']
+                    ],
+                    'status' => 'Released',
+                    'tagline' => 'Mischief. Mayhem. Soap.',
+                    'title' => 'Fight Club',
+                    'video' => false,
+                    'vote_average' => 8.4,
+                    'vote_count' => 29512
+                ]
+            ]);
             break;
         case 'reccomendations':
             $url = "https://api.themoviedb.org/3/movie/{$parameter}/recommendations?language=en-US&page=1";
@@ -137,9 +180,10 @@ while ($channel->is_consuming()) {
 closeRabbit($connection, $channel);
 echo "RabbitMQ connection closed\n";
 
-function fetchDetails($type, $parameter, $url) {
+function fetchDetails($type, $parameter, $url)
+{
     echo "Starting fetchDetails for $type with parameter $parameter\n";
-    
+
     try {
         // Call the API to get a response
         $client = new \GuzzleHttp\Client();
@@ -152,18 +196,18 @@ function fetchDetails($type, $parameter, $url) {
 
         // Get the response body as a string
         $responseBody = $response->getBody()->getContents();
-        
+
         // Decode it to check the structure
         $decodedResponse = json_decode($responseBody, true);
-        
+
         error_log("API Response Body: " . print_r($decodedResponse, true));
-        
+
         // Return the properly structured response
         return json_encode([
             'type' => 'success',
             'data' => $decodedResponse
         ]);
-        
+
     } catch (RequestException $e) {
         // Rest of error handling remains the same
         echo "API request failed\n";
@@ -178,7 +222,7 @@ function fetchDetails($type, $parameter, $url) {
                 ]);
             }
         }
-        
+
         error_log("Error fetching details: " . $e->getMessage());
         return json_encode([
             'type' => 'failure',
