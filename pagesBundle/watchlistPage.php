c3aae0dccaca79c8f2349407e1700041bd30f8ad
alentomjames <atj2@njit.edu>
2024-12-19 20:34:15 -0500

    Update watchlistPage.php

--- pagesBundle/watchlistPage.php
+++ pagesBundle/watchlistPage.php
@@ -24,27 +24,34 @@ $userName = $loggedIn ? $_SESSION['name'] : null;
 </head>
 
 <body>
-    <nav class="navbar">
-        <a href=../index.php" class="nav-title">BreadWinners</a>
+<nav class="navbar">
+        <a href="index.php" class="nav-title">BreadWinners</a>
+
+        <button class="hamburger" aria-label="Toggle navigation">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </button>
+
+
         <ul class="nav-links">
-            <?php if ($loggedIn): ?>
-                <li>
-                    <button onclick="location.href='Reccomend.php'" class="smoothie-button">
-                        <img src="smoothie.png" alt="Movie Smoothie" class="smoothie-icon">
-                    </button>
-                </li>
-                <li><button onclick="location.href='recBasedonLikesPage.php'">Recommended Movies</button></li>
-                <li><button onclick="location.href='MovieTrivia.php'">Movie Trivia</button></li>
-                <li><button onclick="location.href='watchlistPage.php'">Watch Later</button></li>
-                <li><button onclick="location.href='topTenPage.php'">Top Movies</button></li>
-                <p class="nav-title">Welcome, <?php echo htmlspecialchars($userName); ?>!</p>
-                <li><button onclick="location.href='../loginBundle/logout.php'">Logout</button></li>
-            <?php else: ?>
-                <li><button onclick="location.href='../loginBundle/login.php'">Login</button></li>
-                <li><button onclick="location.href='../loginBundle/sign_up.php'">Sign Up</button></li>
-            <?php endif; ?>
-        </ul>
-    </nav>
+        <?php if ($loggedIn): ?>
+            <li>
+                <button onclick="location.href='/pagesBundle/Reccomend.php'" class="smoothie-button">
+                    <img src="smoothie.png" alt="Movie Smoothie" class="smoothie-icon">
+                </button>
+            </li>
+            <li><button onclick="location.href='/pagesBundle/recBasedonLikesPage.php'">Recommended Movies</button></li>
+            <li><button onclick="location.href='/pagesBundle/MovieTrivia.php'">Movie Trivia</button></li>
+            <li><button onclick="location.href='/pagesBundle/watchlistPage.php'">Watch Later</button></li>
+            <li><button onclick="location.href='/pagesBundle/topTenPage.php'">Top Movies</button></li>
+            <li><button onclick="location.href='/loginBundle/logout.php'">Logout</button></li>
+        <?php else: ?>
+            <li><button onclick="location.href='/loginBundle/login.php'">Login</button></li>
+            <li><button onclick="location.href='/loginBundle/sign_up.php'">Sign Up</button></li>
+        <?php endif; ?>
+    </ul>
+</nav>
 
     <h1>Your Watchlist</h1>
     <div class="watchlist-container">
